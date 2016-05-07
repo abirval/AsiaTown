68186916bd5e47c2f7c3471d04b472556427ea73
Ivan Lebedev <degity@gmail.com>
2016-05-07 16:44:12 +0600

    Footer. Законченный

--- static/main.js
+++ static/main.js
@@ -1,36 +1,36 @@
 window.addEventListener('load', function () {
-	//Функция параласка. topi- начальное положение эелемента по высоте
+  //Функция параласка. topi- начальное положение эелемента по высоте
   function paralax(elem, speed, topi) {
     var scroll_amount = (window.scrollY) * speed,
         scroll_amount= scroll_amount + topi;
     elem.style.top = scroll_amount + "px";
   }
 
-	//Функция вращения элемента при скроле. Спид регулируетугол
-	function rotateEl(elem, speed){
-            	var scrolled = window.scrollY * speed;
-            	elem.style.transform = 'rotate('+ scrolled +'deg)';
-	}
+  //Функция вращения элемента при скроле. Спид регулируетугол
+  function rotateEl(elem, speed){
+    var scrolled = window.scrollY * speed;
+    elem.style.transform = 'rotate('+ scrolled +'deg)';
+  }
 
-	var rotate1 = document.querySelector('.rotate-1'),
+  var rotate1 = document.querySelector('.rotate-1'),
 	    rotate2 = document.querySelector('.rotate-2'),
 	    rotate3 = document.querySelector('.rotate-3');
 
 
-	window.addEventListener ('scroll', function () {
-	                paralax(rotate1, 0.1, 20);
-	                rotateEl(rotate1, 0.25);
-	            }, false);
+  window.addEventListener ('scroll', function () {
+    paralax(rotate1, 0.1, 20);
+    rotateEl(rotate1, 0.25);
+  }, false);
 
-	window.addEventListener ('scroll', function () {
-	                paralax(rotate2, 0.3, 150);
-	                rotateEl(rotate2, 0.3);
-	            }, false);
+  window.addEventListener ('scroll', function () {
+    paralax(rotate2, 0.3, 150);
+    rotateEl(rotate2, 0.3);
+  }, false);
 
-	window.addEventListener ('scroll', function () {
-	                paralax(rotate3, 0.1, 750);
-	                rotateEl(rotate3, 0.2);
-	            }, false);
+  window.addEventListener ('scroll', function () {
+    paralax(rotate3, 0.1, 750);
+    rotateEl(rotate3, 0.2);
+  }, false);
 
 	$('.menu-item').on('click', function () {
 		var item = $(this),
@@ -46,4 +46,19 @@ window.addEventListener('load', function () {
 			.siblings()
 			.removeClass('active');
 	});
+
+  // $(window).scroll(function () {
+  //   console.log($('.rotate-1'));
+  //   $('.rotate-1').animate({
+  //     top: '+=10px'
+  //   }, 1);
+  //   });
+
+
+  $('a').click(function(){
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
 });
